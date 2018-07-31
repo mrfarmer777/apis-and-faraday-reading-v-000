b8217d557297df990100cfc32f508ef74a99cc01
Matt Farmer <mrfarmer777@gmail.com>
2018-07-31 01:31:30 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,8 +6,8 @@ class SearchesController < ApplicationController
     begin
       @resp=Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
         #add this stuff to the requests that will result from the code above
-        req.params['client_id']='EQTX1NQG0J0L00R3IO3HYS2D4JCMUBZ0JRS05GBQ1DJJAKJG'
-        req.params['client_secret']='QOC02PHQNYVRBZEBPSEFET51BDX3X1RXF0FSLOLEOWS444VN'
+        req.params['client_id']=''
+        req.params['client_secret']=''
         req.params['v']='20160201'
         req.params['near']=params[:zipcode]
         req.params['query']='coffee shop'
